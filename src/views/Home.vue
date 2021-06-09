7487483da4f4361c1245604253d2988f5ec4ae15
MizunoKazuki <mizukazu.work@gmail.com>
2021-06-09 23:01:59 +0900

    update:ホーム画面に記事を出さない

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,8 @@
   <v-app>
     <v-container>
       <v-row>
-        <v-col v-bind:cols="col" sm="6" lg="4" v-for="(post, index) in postData" :key="index">
-          <router-link :to="{ name:'post', params:{ name: post.title }}" class="card-link">
-            <v-card class="mx-auto" max-width="400">
-            <v-img class="white--text align-end" height="200px" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-              <v-card-title>{{ post.title }}</v-card-title>
-            </v-img>
-
-            <v-card-subtitle class="pb-0">{{ post.date }}</v-card-subtitle>
-            <v-card-subtitle class="pb-0">{{ post.category }}</v-card-subtitle>
-
-            <v-card-text class="text--primary">
-              <div></div>
-            </v-card-text>
-          </v-card>
-          </router-link>
+        <v-col cols="12">
+          Vue製のWikiです。
         </v-col>
       </v-row>
     </v-container>
