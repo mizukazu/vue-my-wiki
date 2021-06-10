7786eb99ea3549b76e6243c0df9e317e33e666d4
MizunoKazuki <mizukazu.work@gmail.com>
2021-06-10 21:33:01 +0900

    update:スクリプトの修正

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -6,47 +6,54 @@ const { JSDOM } = jsdom
 const marked = require('marked')
 const postPath = 'src/assets/data/post.json'
 const postDirPath = 'src/assets/data/post'
+const navigationPath = 'src/config/navigation.json'
 const fo = require('./modules/fileOperation')
 
 // ファイルの一覧を取得
-const fileList = fo.getFileList(postDirPath).filter(file => { return file.includes('.md') })
+// const fileList = fo.getFileList(postDirPath).filter(file => { return file.includes('.md') })
+// console.log(fileList)
+const navigation = JSON.parse(fo.readFile(navigationPath))
 
-console.log(fileList)
 
 // 記事データから必要な情報を抽出
 const postJson = []
-
+const category = []
 glob(`${postDirPath}/**/*`, (err, files) => {
-  const category = []
   files.forEach(file => {
-    // const split = file.split('/').pop()
     if (!file.split('/').pop().includes('.md')) {
       category.push(file.split('/').pop())
+    } else {
+      const data = file.includes('.md') ? marked(fo.readFile(file)) : ''
+      const title = file.split('/').pop().split('.')[0]
+      const postCategory = file.split('/')[4].toUpperCase()
+      console.log(data)
+      postJson.push({
+        title: title,
+        category: postCategory,
+        content: data,
+        fullPath: file
+      })
     }
-    const data = file.includes('.md') ? marked(fo.readFile(file)) : ''
-    console.log(data)
-    postJson.push({
-      content: data,
-      fullPath: file
-    })
   })
-  // 記事データのJSONを作成
-  fo.writeFile(postPath, JSON.stringify(postJson, null, '\t'))
-  console.log(postJson)
+category.forEach(cat => {
+  navigation[1].childs.forEach(child => {
+    console.log(cat.toUpperCase(), child.name)
+    if(child.name.includes(cat.toUpperCase())) {
+      child.posts = []
+    }
+  })
+})
+postJson.forEach(post => {
+  const p = navigation[1].childs.find(category => category.name.localeCompare(post.category))
+  const navPost = {
+    title: post.title,
+    link: `/${post.category.toLowerCase()}/${post.title}`
+  }
+  p.posts.push(navPost)
 })
 
-// fileList.forEach(file => {
-//   // markdown形式のファイルをHTMLに変換
-//   const data = marked(fo.readFile(postDirPath + '/' + file))
-//   const dom = new JSDOM(data)
-//   const postDate = data.match(/(\d{4})\/(\d{2})\/(\d{2})/)[0]
-//   const postCategory = data.split('\n')[1].slice(3).slice(0, -4)
-//   const postTitle = dom.window.document.querySelector('h1').textContent
-//   postJson.push({
-//     date: postDate,
-//     title: postTitle,
-//     content: data,
-//     category: postCategory,
-//     fileName: file
-//   })
-// })
+  // 記事データのJSONを作成
+  fo.writeFile(postPath, JSON.stringify(postJson, null, '\t'))
+  fo.writeFile(navigationPath, JSON.stringify(navigation, null, '\t'))
+  // console.log(category)
+})
